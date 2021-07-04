c56ff5c4bfee29dbd0297ae1304977553d4f3e8d
mikalacki <milos.mikalacki5@gmail.com>
2021-07-05 01:38:03 +0200

    added User class that represent one user that we got form database.

--- app/src/main/java/com/example/mmreviews/MainActivity.java
+++ app/src/main/java/com/example/mmreviews/MainActivity.java
@@ -4,22 +4,36 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.internal.Objects;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.mopub.volley.Request;
+import com.mopub.volley.Response;
+import com.mopub.volley.VolleyError;
+import com.mopub.volley.toolbox.StringRequest;
+import com.mopub.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String USER_URL = "http://192.168.1.59/LoginRegister/myApi.php";
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadUsers();
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -51,4 +65,33 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+    private void loadUsers(){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, USER_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                try {
+                    JSONArray users = new JSONArray(s);
+                    for (int i = 0; i < s.length(); i++){
+                        JSONObject userObject = users.getJSONObject(i);
+
+                        int userId = userObject.getInt("userID");
+                        String username = userObject.getString("username");
+                        String fullname = userObject.getString("fullName");
+                        String email = userObject.getString("email");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(MainActivity.this, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Volley.newRequestQueue(this, null).add(stringRequest);
+    }
+
 }
